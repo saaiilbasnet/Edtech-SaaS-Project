91ba50f3be1752c74e91c26901eac16beec157b0
Saaiil Basnet <basnetssahil@gmail.com>
2025-05-30 21:47:04 +0545

    table created

--- Server/src/database/connection.ts
+++ Server/src/database/connection.ts
@@ -1,11 +1,13 @@
-import {Sequelize} from 'sequelize'
+import { error } from 'console';
+import {Sequelize} from 'sequelize-typescript'
 const sequelize = new Sequelize({
     database : process.env.DB_NAME,
     username : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
     host : process.env.DB_HOST,
     dialect : 'mysql',
-    port : Number(process.env.DB_PORT)
+    port : Number(process.env.DB_PORT),
+    models : [__dirname + '/models']
 })
 
 sequelize.authenticate()
@@ -16,3 +18,14 @@ sequelize.authenticate()
 .catch((error)=>{
     console.log(error);
 })
+
+//migration code
+
+sequelize.sync({alter:true}).then(()=>{
+    console.log("Successfully Migrated to Database");
+}).catch((error)=>{
+    console.log(error);
+    
+})
+
+export default sequelize
---
+++ Server/src/database/models/user.model.ts
@@ -0,0 +1,44 @@
+
+import {Table, Column, Model, DataType} from 'sequelize-typescript'
+
+//definig a table
+
+@Table({
+    tableName : 'users',
+    modelName : 'User',
+    timestamps : true
+})
+
+class User extends Model {
+    //defining the column in table
+
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue : DataType.UUIDV4
+    })
+    declare id : string
+
+  @Column({
+    type : DataType.STRING
+  })
+  declare username : string
+
+    @Column({
+    type : DataType.STRING
+  })
+  declare password : string
+
+    @Column({
+    type : DataType.STRING
+  })
+  declare email : string
+
+    @Column({
+    type : DataType.ENUM('teacher','institute','super-admin','student'),
+    defaultValue : 'student'
+  })
+  declare role : string
+}
+
+export default User
