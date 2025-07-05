515da46f4d2585101b87f353ecd2b3d3eb86a703
Saaiil Basnet <basnetssahil@gmail.com>
2025-07-05 11:10:21 +0545

    customHooks for state management added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,24 @@
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+import { setName } from "@/lib/store/userSlice";
 
 export default function Home() {
+
+  // using dispatch
+
+      const name = "Saaiil";
+      const dispatch = useAppDispatch();
+      dispatch(setName(name));
+
+      // using selector
+
+      const data = useAppSelector((store)=> store.userSlice)
+      console.log(data.name);
+      
+
+
   return (
+
     <h1>This is HomePage</h1>
+
   );
 }
--- client/src/lib/store/store.ts
+++ client/src/lib/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './userSlice'
 import studentSlice from './studentSlice'
+import { useDispatch } from "react-redux";
 
 const store = configureStore({
     reducer : {
@@ -11,3 +12,4 @@ const store = configureStore({
 
 export default store
 export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType <typeof store.getState>;
